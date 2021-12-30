5b6d6986342ae423e410785586b50a1648db94bb
Дилян Палаузов <git-dpa@aegee.org>
2021-12-30 10:02:47 +0200

    Clarifications to the deleteMessage() call

--- proxied.d.ts
+++ proxied.d.ts
@@ -1037,13 +1037,13 @@ export interface InputFileProxy<F> {
     }): Poll;
 
     /** Use this method to delete a message, including service messages, with the following limitations:
-    - A message can only be deleted if it was sent less than 48 hours ago.
+    - A message can only be deleted by the Bot API if it was sent less than 48 hours ago.  The “Telegram API” does not have this limitation.
     - A dice message in a private chat can only be deleted if it was sent more than 24 hours ago.
     - Bots can delete outgoing messages in private chats, groups, and supergroups.
     - Bots can delete incoming messages in private chats.
     - Bots granted can_post_messages permissions can delete outgoing messages in channels.
     - If the bot is an administrator of a group, it can delete any message there.
-    - If the bot has can_delete_messages permission in a supergroup or a channel, it can delete any message there.
+    - If the bot has can_delete_messages permission in a supergroup or a channel, it can delete any message there, provided the message is not older than 48 hours.
     Returns True on success. */
     deleteMessage(args: {
       /** Unique identifier for the target chat or username of the target channel (in the format @channelusername) */
