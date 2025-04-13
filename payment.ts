17d34d91ff0bf18b792dd8fefc70034b5b2dbac3
KnorpelSenf <shtrog@gmail.com>
2025-04-13 20:10:09 +0200

    docs: sync

--- payment.ts
+++ payment.ts
@@ -422,7 +422,7 @@ export interface PaidMessagePriceChanged {
 export interface StarAmount {
   /** Integer amount of Telegram Stars, rounded to 0; can be negative */
   amount: number;
-  /** The number of 1/1000000000 shares of Telegram Stars; from -999999999 to 999999999; can be negative if amount is negative */
+  /** The number of 1/1000000000 shares of Telegram Stars; from -999999999 to 999999999; can be negative if and only if amount is non-positive */
   nanostar_amount?: number;
 }
 
