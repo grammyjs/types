4e1dcba37634301c25cb74e7ca6a55d7e4eb59f1
KnorpelSenf <shtrog@gmail.com>
2026-06-13 16:54:19 +0200

    fix: RichText type

--- rich.ts
+++ rich.ts
@@ -324,6 +324,8 @@ export interface InputRichMessage {
 - RichTextReference
 - RichTextReferenceLink */
 export type RichText =
+  | string
+  | RichText[]
   | RichTextBold
   | RichTextItalic
   | RichTextUnderline
