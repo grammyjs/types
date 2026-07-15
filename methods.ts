3d799313b8efdafa455be5d56355e43eec9cdaf4
KnorpelSenf <shtrog@gmail.com>
2026-07-15 09:09:24 +0200

    fix: captions are optional
    Co-authored-by: Heero <github@heeroo.dev>

--- methods.ts
+++ methods.ts
@@ -3119,7 +3119,7 @@ export interface InputMediaVoiceNote<F> {
   /** File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended), pass an HTTP URL for Telegram to get a file from the Internet, or pass "attach://\<file_attach_name>" to upload a new one using multipart/form-data under \<file_attach_name> name. More information on Sending Files » */
   media: F | string;
   /** Caption of the voice message to be sent, 0-1024 characters after entities parsing */
-  caption: string;
+  caption?: string;
   /** Mode for parsing entities in the voice message caption. See formatting options for more details. */
   parse_mode?: ParseMode;
   /** List of special entities that appear in the caption, which can be specified instead of parse_mode */
